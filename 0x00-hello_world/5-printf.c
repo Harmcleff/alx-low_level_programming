eca0233a82e6229c5c03bde6997014aed675a6b9
hammed <sanyaolu.oladimeji@gmail.com>
2022-10-14 03:16:34 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 /**
  * Main - Entry point
  *
- * Description: 'the program's description
- * '
  * Return: 0
  */
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
