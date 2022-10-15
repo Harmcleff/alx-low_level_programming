d93b0b6680160f8e90ef47d998ae5180413c5a9a
hammed <sanyaolu.oladimeji@gmail.com>
2022-10-14 21:14:11 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,21 +2,21 @@
 /**
  * main - prints all possible different combinations of two digits.
  * Return: Always 0 (Success)
- * */
+ */
 int main(void)
 {
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
+		for (ones = '0'; ones <= '9'; ones++)
 		{
-			if (!((ones == tens) || (tens > ones)))/*eliminates repeatition*/
+			if (!((ones == tens) || (tens > ones)))
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == '9' && tens == '8'))/*addes comma and space*/
+				if (!(ones == '9' && tens == '8'))
 				{
 					putchar(',');
 					putchar(' ');
