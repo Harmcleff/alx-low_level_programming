b578ff783a02cdd27c0e7df4c3d4ce9880f52437
hammed <sanyaolu.oladimeji@gmail.com>
2022-10-14 20:55:36 -0700

    bas16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,19 +5,13 @@
  */
 int main(void)
 {
-	int num = '0';
-	char letter = 'a';
+	int d;
+	char low;
 
-	while (num <= '9')/*print 0-9*/
-	{
-		putchar(num);
-		num++;
-	}
-	while (letter <= 'f')/*print a to f*/
-        {
-		putchar(letter);
-		letter++;
-	}
+	for (d = '0'; d <= '9'; d++)
+		putchar (d);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
 	putchar('\n');
 	return (0);
 }
