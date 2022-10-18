c8d886213c81f0d980562e126fe5f39bdbd72efc
hammed <sanyaolu.oladimeji@gmail.com>
2022-10-18 15:17:09 -0700

    alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,12 @@
  */
 void print_alphabet_x10(void)
 {
-	int low;
+	char i;
+	int j;
 
-	for (low = 'a'; low <= 'z'; low++)
-	_putchar(low);
-	_putchar(low);
-	_putchar(low);
-	_putchar(low);
-	_putchar(low);
-	_putchar(low);
-	_putchar(low);
-	_putchar(low);
-	_putchar(low);
-	_putchar(low);
+	for (j = 0; j < 10'; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
+	_putchar('\n');
 }
