a03fc62a31bf4898f610272149b93a854c8b2126
hammed <sanyaolu.oladimeji@gmail.com>
2022-10-19 14:58:38 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,8 +5,7 @@
  * main - Entry point
  *
  * Description: computes the of sum all
- * the multiples of 3 or 5 
- * below 1024 (excluded)
+ * the multiples of 3 or 5 below 1024 (excluded)
  *
  * Return: Always 0 (Success)
  */
