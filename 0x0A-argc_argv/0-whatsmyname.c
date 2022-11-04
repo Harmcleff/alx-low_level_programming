bb70b0224c76d33ddb4b9cf2c7e9b08eabb6bb6f
hammed <sanyaolu.oladimeji@gmail.com>
2022-11-04 11:00:28 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ *
+ * return: Always 0 
+ */
+int main (int argc, char* argv[])
+{
+	printf ("%s\n",argv[0]);
+	return (0);
+}
