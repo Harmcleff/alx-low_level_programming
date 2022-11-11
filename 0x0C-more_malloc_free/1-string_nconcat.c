8cd1562e0185451258ea6e043c1c6992717fc8ec
Harmcleff <33807817+Harmcleff@users.noreply.github.com>
2022-11-11 02:10:40 +0100

    str

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,42 +11,34 @@
  * Return: If the function fails - NULL.
  *         Otherwise - a pointer to the concatenated space in memory.
  */
- char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *result;
-    unsigned int len = n,i;
+	char *concat;
+	unsigned int len = n, index;
 
-    if (s1 == NULL)
-    {
-          s1 = "";
-    }      
-    
-     if (s2 == NULL)
-    {
-          s2 = "";
-    }      
-      
-    for (i = 0; s1[i]; i++)
-            len++;
+	if (s1 == NULL)
+		s1 = "";
 
-    result = malloc(sizeof(char) * (len + 1));
-  
-    if (result == NULL)
-    {
-        return NULL;
-    }
+	if (s2 == NULL)
+		s2 = "";
 
-    len = 0;
+	for (index = 0; s1[index]; index++)
+		len++;
 
-    for (i = 0; s1[i]; i++)
-        result[len++] = s1[i];
+	concat = malloc(sizeof(char) * (len + 1));
 
-    
+	if (concat == NULL)
+		return (NULL);
 
-    for (i = 0; s2[i] && i < n; i++)
-        result[len++] = s2[i];
-    
-   result[len] = '\0';
+	len = 0;
 
-    return (result);
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
