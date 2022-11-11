c7ca4060e7315ba5dcc01437be7fddf62030e71f
hammed <sanyaolu.oladimeji@gmail.com>
2022-11-10 17:06:49 -0800

    string

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,16 +3,15 @@
 /*
  * malloc_checked - Allocates memory using malloc.
  * @b: The number of bytes to be allocated
+ *
  * Return - A pointer to the allocated memory.
  */
 void *malloc_checked(unsigned int b)
 {
 	 void *space = malloc(b);
-	 
-	 if (space == NULL)
-	{
-		exit(98);
-	}
+
+	 if (mem == NULL)
+		 exit(98);
 
 	return (*space);
 }
