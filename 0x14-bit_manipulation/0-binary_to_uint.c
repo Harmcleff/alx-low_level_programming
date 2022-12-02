a6b4de55159ebd2ca807586b9ef8696dddfe4b57
Harmcleff <33807817+Harmcleff@users.noreply.github.com>
2022-12-03 00:27:20 +0100

    binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int ub;
+	int len, base_two;
+
+	if (!b)
+		return (0);
+
+	ui = 0;
+
+	while (b[len])
+  {
+    len++;
+  }
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ub += base_two;
+		}
+	}
+
+	return (ui);
+}
